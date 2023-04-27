650de789cb4030c5391af2f415c7bf8e62da97ed
unknown <priyanka.dombale@finizon.com>
2023-04-27 17:25:33 +0530

    [Priyanka]:dist folder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   // ServerUrl: "http://127.0.0.1:8000/api/",
 
   // by sw 
-  ServerUrl: "http://65.1.237.83/api",
+  ServerUrl: "http://65.1.237.83/api/",
   
   // ServerUrl: "http://15.206.79.170:8080/api/",
 
