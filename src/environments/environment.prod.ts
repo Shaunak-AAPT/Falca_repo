3e184028f9aa8f0c28099241567ec69d5942cef4
unknown <priyanka.dombale@finizon.com>
2023-10-13 18:42:15 +0530

    [Priyanka]Added:Fixed the header and mobile view

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
 
 
   // dev
-  ServerUrl: "http://65.1.237.83/api/",
+  // ServerUrl: "http://65.1.237.83/api/",
 
   // ServerUrl: "https://dev.finizon.com/api/",
-  // ServerUrl: "https://uatnw.finizon.com/api/",
+  ServerUrl: "https://uatnw.finizon.com/api/",
   // ServerUrl: "https://uatnw.finizon.com/api/",
 
 
