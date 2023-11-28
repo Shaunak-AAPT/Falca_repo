d5effa6f47bc223d9634f71e3d6f92f079ba1405
Shaunak-AAPT <shaunak.wankhade@finizon.com>
2023-11-28 23:06:58 +0530

    [shaunak]added: common sonarchanges

--- src/app/views/shared/header/header.component.ts
+++ src/app/views/shared/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component,OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-// import { registerRequest } from 'src/app/models/registerRequest.model';
 import { ApiService } from 'src/app/services/api/api.service';
 import { AESCryptoService } from 'src/app/services/cryptomanager/aescrypto.service';
 import { EligibilityService } from 'src/app/services/eligibility/eligibility.service';
@@ -50,14 +49,7 @@ export class HeaderComponent implements OnInit {
   urlValidateOTP: any = 'auth/customer/ValidateOTP';
   AgentInsuranceUrl = environment.AgentInsuranceUrl;
   AgentCommonUrl = environment.AgentCommonUrl;
-  // @ViewChild(SupportModalComponent) supportComponent!: SupportModalComponent;
-  // @ViewChild('supportModal') supportModal!: ElementRef;
-  // @Output() supportClicked = new EventEmitter<void>();
-
-  // onSupportClick() {
-  //   this.supportClicked.emit();
-  //   console.log( this.supportClicked.emit(),'supportemit')
-  // }
+ 
 
   constructor(public validation: ValidateService, private toastr: ToastrService, private route: Router, private api: ApiService, private cryptoManager: AESCryptoService, private eligibility: EligibilityService) { }
 
@@ -156,112 +148,7 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  otpToggle(event: Event, calltype?: any) {
-    // event.stopPropagation();
-    // if (this.validation.isNullEmptyUndefined(this.FormEmail)) {
-    //   this.toastr.error("Please enter a valid email");
-    // }
-    // else if (!this.validation.validateEmail(this.FormEmail)) {
-    //   this.toastr.error("Please enter valid Email id");
-    // }
-    // else {
-
-    //   this.ResetOTP();
-    //   setTimeout(() => {
-    //     if (calltype == 'login') {
-    //       this.calltype = 'login';
-    //       this.Login(event);
-    //     }
-    //     else {
-    //       this.calltype = 'register';
-    //       this.RegisterCust(event);
-    //     }
-    //   }, 0);
-
-    // }
-  }
-
-  Login(event: Event) {
-    // try {
-    //   event.stopImmediatePropagation();
-    //   let loginData = new FormData();
-    //   loginData.append('email', this.FormEmail);
-    //   this.api.post('auth/customer/login', loginData, false).subscribe(async response => {
-    //     if (response.response.n == 1) {
-    //       this.isOTPSent = true;
-    //       $("#headerotp-screen").modal("show");
-    //       this.resendbuttonText = "0:60"
-    //       this.countdown();
-    //       // console.log('response', response)
-    //       this.toastr.success(response.response.Msg);
-    //     }
-    //     else {
-    //       this.toastr.error(response.response.Msg);
-    //       this.isOTPSent = false;
-         
-    //     }
-    //   });
-    // }
-    // catch (ex) {
-    // }
-  }
-
-  RegisterCust(event: Event) {
-
-    // if (this.validation.isNullEmptyUndefined(this.FormFirstName)) {
-    //   this.toastr.error("First Name is Mandatory");
-    // }
-    // else if (this.validation.isNullEmptyUndefined(this.FormLastName)) {
-    //   this.toastr.error("Last Name is Mandatory");
-    // }
-    // else if (this.validation.isNullEmptyUndefined(this.FormEmail)) {
-    //   this.toastr.error("Email id is Mandatory");
-    // }
-    // else if (!this.validation.validateEmail(this.FormEmail)) {
-    //   this.toastr.error("Please enter valid Email id");
-    // }
-    // else if (this.validation.isNullEmptyUndefined((this.FormMobileNo.toString()).trim())) {
-    //   this.toastr.error("Mobile Number is Mandatory");
-    // }
-    // else if (!this.validation.validateMobileNumber(this.FormMobileNo.trim())) {
-    //   this.toastr.error("Please enter valid Mobile number");
-    // }
-    // else {
-    //   if (this.usertype == 'Customer') {
-    //     this.UrlRegister = 'auth/customer/register';
-    //   }
-    //   else if (this.usertype == 'Partner') {
-    //     this.UrlRegister = 'auth/agent/register';
-    //   }
-
-    //   try {
-    //     event.stopImmediatePropagation();
-    //     let registerData = new registerRequest();
-    //     registerData.mobileNumber = this.FormMobileNo;
-    //     registerData.email = this.FormEmail;
-    //     registerData.firstName = this.FormFirstName;
-    //     registerData.lastName = this.FormLastName;
-    //     registerData.consent_id = 1;
-    //     registerData.ip_address = '192.168.0.1';
-    //     registerData.gender = 1;
-
-    //     this.api.post(this.UrlRegister, registerData, false).subscribe(async response => {
-    //       if (response.response.n == 1) {
-    //         this.isOTPSent = true;
-    //         $("#headerotp-screen").modal("show");
-    //         this.resendbuttonText = "0:60"
-    //         this.countdown();
-    //         this.toastr.success(response.response.Msg);
-    //       }
-    //       else {
-    //         this.toastr.error(response.response.Msg);
-    //         this.isOTPSent = false;
-    //       }
-    //     });
-    //   } catch (ex) {
-    //   }
-    // }
-  }
+  
 
   Logout() {
     this.isLoggedIn = false;
@@ -272,51 +159,7 @@ export class HeaderComponent implements OnInit {
     this.route.navigate(['/']);
   }
 
-  ResendOTP() {
-    // if (this.usertype == 'Customer') {
-    //   this.UrlSendOTP = 'auth/customer/send-otp';
-    // }
-    // else if (this.usertype == 'Partner') {
-    //   this.UrlSendOTP = 'auth/agent/loginWithOtp';
-    // }
 
-    // this.ResetOTP();
-    // const data = new FormData();
-    // data.append("email", this.FormEmail);
-    // this.api.post(this.UrlSendOTP, data, false).subscribe(response => {
-    //   if (response.response.n == 1) {
-    //     this.resendbuttonText = "0:60";
-    //     this.countdown();
-    //   }
-    //   else {
-    //     this.toastr.error(response.response.Msg);
-    //   }
-    // })
-  }
-
-  countdown() {
-    // clearInterval(this.interval);
-    // this.interval = setInterval(() => {
-    //   let timer: any = this.resendbuttonText;
-    //   timer = timer.split(':');
-    //   let minutes = timer[0];
-    //   let seconds = timer[1];
-    //   seconds -= 1;
-    //   if (minutes < 0) return;
-    //   else if (seconds < 0 && minutes != 0) {
-    //     minutes -= 1;
-    //     seconds = 59;
-    //   }
-    //   else if (seconds < 10 && seconds.length != 2) {
-    //     seconds = '0' + seconds
-    //   }
-    //   this.resendbuttonText = minutes + ':' + seconds;
-    //   if (minutes == 0 && seconds == 0) {
-    //     clearInterval(this.interval)
-    //     this.resendbuttonText = 'Resend OTP';
-    //   };
-    // }, 1000);
-  }
 
   ResetOTP() {
     this.headotp1 = "";
@@ -327,130 +170,8 @@ export class HeaderComponent implements OnInit {
     this.headotp6 = "";
   }
 
-  keytab(nextTabId: number, event: any) {
-    // let actionFlag = false;
-    // const charCode = (event.which) ? event.which : event.keyCode;
-    // if (charCode > 31 && (charCode < 48 || charCode > 57) && (charCode < 96 || charCode > 105)) {
-    // console.log("empty");
-    // }
-    // else if (charCode == 8) {
-    //   if (nextTabId < 1) {
-    //     nextTabId = 1;
-    //   }
-    //   nextTabId -= 2;
-    //   actionFlag = true;
-    // }
-    // else {
-    //   if (nextTabId > 6) {
-    //     nextTabId = 6;
-    //   }
-    //   actionFlag = true;
-    // }
-    // // console.log("OTPfssf0",$('#headotp'+(nextTabId -1)).val().length)
-    // if (actionFlag && $('#headotp' + (nextTabId - 1)).val().length == 1) {
-    //   const field = document.getElementById("headotp" + nextTabId);
-    //   if (field) {
-    //     field.focus();
-    //     field.click();
-    //   }
-    // }
-  }
 
-  async verifyOtpBtn() {
-    await this.verifyOtp();
-  }
-
-  async verifyOtp() {
-    // if (this.usertype == 'Customer') {
-    //   this.urlValidateOTP = 'auth/customer/ValidateOTP';
-    // }
-    // else if (this.usertype == 'Partner') {
-    //   this.urlValidateOTP = 'auth/agent/loginOtpValidator';
-    // }
-
-    // return new Promise(async (resolve, reject) => {
-    //   let otp = this.headotp1.toString() + this.headotp2.toString() + this.headotp3.toString() + this.headotp4.toString() + this.headotp5.toString() + this.headotp6.toString();
-
-    //   if (!this.validation.isNullEmptyUndefined(this.FormEmail) && otp.length == 6) {
-    //     const data = new FormData();
-    //     data.append("email", this.FormEmail);
-    //     data.append("otp", otp); //this.otp1 + this.otp2 + this.otp3 + this.otp4 + this.otp5 + this.otp6);
-
-    //     this.api.post(this.urlValidateOTP, data).subscribe(async response => {
-    //       if (response.response.n == 1) {
-    //         this.toastr.success("OTP Validation Success");
-    //         $("#headerotp-screen").modal("hide");
-    //         this.isLoggedIn = true;
-    //         if (!this.validation.isNullEmptyUndefined(response.data.token.customer)) {
-    //           let encryptedTokenCustomer = { "token": response.data.token.customer };
-    //           localStorage.setItem("CustToken", this.cryptoManager.Encrypt(encryptedTokenCustomer));
-    //         }
-    //         if (!this.validation.isNullEmptyUndefined(response.data.token.agent)) {
-    //           let encryptedTokenAgent = { "token": response.data.token.agent };
-    //           localStorage.setItem("AgentToken", this.cryptoManager.Encrypt(encryptedTokenAgent));
-    //         }
-    //         this.ResetModal();
-    //         this.GetApplicantData();
-    //         if (!this.validation.isNullEmptyUndefined(response.data.token.agent)) {
-              
-    //           if (response.agentType.Insurance == true && (response.agentType.credit == true || response.agentType.wealth == true)) {
-    //             this.GoToAgentCommon('agent-overview');
-    //           }
-    //           else if (response.agentType.Insurance == true) {
-    //             if (response.agentKyc.Insurance == true) {
-    //               this.GoToAgentInsurance('/agent-insurance-dashboard');
-    //             }
-    //             else {
-    //               this.GoToAgentInsurance('/agent-insurance-onboarding');
-    //             }
-    //           }
-    //           else if (response.agentType.Insurance != true) {
-
-    //             if (response.agentType.credit == true && response.agentType.wealth == true) {
-    //               this.GoToAgentCommon('agent-overview');
-    //             }
-    //             else if (response.agentType.credit == true) {
-    //               if (response.agentKyc.credit == true) {
-    //                 this.GoToAgentCommon('agent-credit-dashboard');
-    //               }
-    //               else {
-    //                 this.GoToAgentCommon('agent-credit-onboarding');
-    //               }
-    //             }
-    //             else if (response.agentType.wealth == true) {
-    //               if (response.agentKyc.wealth == true) {
-    //                 this.GoToAgentCommon('agent-wealth-dashboard');
-    //               }
-    //               else {
-    //                 this.GoToAgentCommon('agent-wealth-onboarding');
-    //               }
-    //             }
-    //             else {
-    //               this.GoToAgentCommon('agent-type');
-    //             }
-    //           }
-    //         }
-    //         else if (this.validation.isNullEmptyUndefined(response.data.token.agent)) {
-    //           setTimeout(() => {
-    //             if (this.route.url == '/') {
-    //               this.route.navigate(['/overview']);
-    //             }
-    //           }, 1000);
-    //         }
-
-    //         resolve(response);
-    //       }
-    //       else {
-    //         this.toastr.error(response.response.Msg);
-    //         reject(response);
-    //       }
-    //     });
-    //   } else {
-    //     this.toastr.error("Please enter valid OTP.");
-    //     reject(0);
-    //   }
-    // });
-  }
+  
 
   ResetModal() {
     this.FormFirstName = "";
