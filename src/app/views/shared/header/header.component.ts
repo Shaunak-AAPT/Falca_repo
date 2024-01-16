4febd0c6a2f1d0afb6aa3d234e4f1f057c7f71c6
Shaunak-AAPT <shaunak.wankhade@finizon.com>
2024-01-16 15:08:52 +0530

    [shaunak]added: regex changes and cyber insurance product icon for uat

--- src/app/views/shared/header/header.component.ts
+++ src/app/views/shared/header/header.component.ts
@@ -201,10 +201,7 @@ export class HeaderComponent implements OnInit {
   }
   
   wealthlink() {
-    // window.location.href = "https://uatnw.finizon.com/wealth";
-    window.location.href = "http://65.1.237.83/wealth";
-
-    
+    window.location.href = "https://uatnw.finizon.com/wealth";    
   }
 
 
@@ -217,26 +214,16 @@ export class HeaderComponent implements OnInit {
 
   }
   insurefitLink() {
-        // window.open("https://dev.finizon.com/spprd/wellness?referralid=goqii", "_blank", "noopener");
-        window.open(" http://65.1.237.83:5800/spprd/wellness?referralid=goqii", "_blank", "noopener");
-     
+        window.open("https://dev.finizon.com/spprd/wellness?referralid=goqii", "_blank", "noopener");     
   }
 
   shopkeeperLink() {
-    // window.open("https://dev.finizon.com/spprd/shopkeeper?referralid=godigit", "_blank", "noopener");
-    window.open("http://65.1.237.83:5800/spprd/shopkeeper?referralid=godigit", "_blank", "noopener");
-
+    window.open("https://dev.finizon.com/spprd/shopkeeper?referralid=godigit", "_blank", "noopener");
   }
   monexoLink() {
-    // window.open("https://dev.finizon.com/monexo?referralid=monexo", "_blank", "noopener");
-    window.open("http://65.1.237.83:5800/monexo?referralid=monexo", "_blank", "noopener");
-  
+    window.open("https://dev.finizon.com/monexo?referralid=monexo", "_blank", "noopener");  
   }
-
-
   insureLink() {
-    // window.open("https://uatnw.finizoninsurance.com/", "_blank", "noopener");    
-    window.open("http://65.1.237.83:6800/", "_blank", "noopener");   
-    
+    window.open("https://uatnw.finizoninsurance.com/", "_blank", "noopener");    
   }
 }
